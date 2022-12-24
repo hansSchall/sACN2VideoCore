12cc0dc2d6a8e4b273d874a20c98d9f3e338ff13
Hans Schallmoser <99032404+hansSchall@users.noreply.github.com>
2022-12-24 12:36:45 +0100

    .gitignore: include webpack config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/test/test.ts',
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.shader$/,
+                type: "asset/source",
+            },
+        ],
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'sACN2VideoCore',
+            template: 'src/test/test.html'
+        })
+    ],
+
+    output: {
+        filename: '[fullhash].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+};
